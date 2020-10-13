366a6427d655c2c28925b99b831c59b62721c0c2
looskie <50378828+Looskie@users.noreply.github.com>
2020-10-13 09:22:37 -0400

    Indentation fix - Error/success messages - edit/delete post

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,7 @@
 $(window).on('load', () => {
+    console.log('%c%s', 'color:#fbe177; font-size: 25px;', 'Whatcha snoopin\' fo?');
+    console.log('%c%s', 'color:#fbe177; font-size: 13px; font-weight: 200;', '(WEBSITE MADE BY DEVLOOSKIE)');
+    console.error('suck my cock nathan');
     $('#homepicture').addClass('expand');
     $('.thirtypiece-scroller, .two').addClass('fastboygozoooooom');
     setTimeout(() => {
@@ -9,7 +12,7 @@ $(window).on('load', () => {
 
 function play(song) {
     closeOthers();
-    switch(true) {
+    switch (true) {
         case song == 'THIRTYPIECE':
             $('#THIRTYPIECEimg, #THIRTYPIECE').addClass('toggleOff');
             $('#THIRTYPIECEselector').removeClass('toggleOff').addClass('toggleOn');
@@ -38,53 +41,136 @@ function play(song) {
 
 async function postTestimonial() {
     const name = document.getElementById('name').value,
-          country = document.getElementById('country').value,
-          testimonial = document.getElementById('testimonial').value;
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name, country, testimonial})
+        country = document.getElementById('country').value,
+        testimonial = document.getElementById('testimonial').value;
+    if (!name == "" && !testimonial == "" && name.length <= 12) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, country, testimonial })
+        }
+        fetch('/postTestimonial', options)
+            .then(res => res.json())
+            .then(json => {
+                if (json.status == 'success') {
+                    castSuccess('Success!');
+                    localStorage.setItem('ID', json.id);
+                    localStorage.setItem('message', testimonial);
+                    getTestimonial();
+                } else {
+                    castError('An error occured!');
+                }
+            })
+            .catch(err => castError('Error! You\'ve already posted a testimonial!'))
+    } else {
+        if (name == "") {
+            castError('You need to enter a name!');
+        } else if (testimonial == "") {
+            castError('You need to have a testimonial!')
+        } else {
+            castError('Your name is too long! (Over 12 characters)');
+        }
     }
-    fetch('/postTestimonial', options)
-    .then(res => res.json())
-    .then(json => {
-        console.log(json);
-        localStorage.setItem('ID', json.id);
-        getTestimonial();
-    })
-    .catch(err => console.log(`Some type of error happened I have no idea what type of error could happen but here it is \n ${err}`))
 }
 
-async function editTestimonial() {
-    const name = document.getElementById('name').value,
-          country = document.getElementById('country').value,
-          testimonial = document.getElementById('testimonial').value,
-          id = localStorage.getItem('ID');
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({name, country, testimonial, id})
+function castError(err) {
+    $(`<div class="error"> <h6 class="error">${err}</h6></div>`).appendTo('.errorsAndSuccesses');
+    setTimeout(() => {
+        $('.error').fadeOut(1500)
+    }, 3000);
+}
+
+function castSuccess(succ) {
+    $(`<div class="success"> <h6 class="success">${succ}</h6></div>`).appendTo('.errorsAndSuccesses');
+    setTimeout(() => {
+        $('.success').fadeOut(1500)
+    }, 3000);
+}
+
+async function checkId(method) {
+    const name = document.getElementById('name'),
+        country = document.getElementById('country'),
+        testimonial = document.getElementById('testimonial'),
+        submitbtn = document.getElementById('submitbtn'),
+        id = localStorage.getItem('ID');
+    if (id) {
+        const checkIdOptions = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: (JSON.stringify({ id })) }
+        fetch('/checkID', checkIdOptions)
+            .then(res => res.json())
+            .then(json => {
+                if (json.status == 'success') {
+                    name.value = json.data[0].name;
+                    country.value = json.data[0].country;
+                    testimonial.value = json.data[0].testimonial;
+                    location.href = "#testipost";
+                    if (method == 'editPost()') {
+                        submitbtn.textContent = 'EDIT POST';
+                        submitbtn.setAttribute('onClick', 'editPost()');
+                    } else {
+                        deletePost();
+                    }
+                } else {
+                    castError('Error! There was a problem getting your post!');
+                }
+            })
+    } else {
+        castError('Couldn\'t find your post!');
     }
-    const res = await fetch('/changeTestimonial', options);
-    const json = await res.json();
-    console.log(json);
-    getTestimonial();
+}
+
+function editPost() {
+    const nameVal = document.getElementById('name').value,
+        countryVal = document.getElementById('country').value,
+        testimonialVal = document.getElementById('testimonial').value,
+        id = localStorage.getItem('ID');
+    const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ nameVal, countryVal, testimonialVal, id }) }
+    fetch('/changeTestimonial', options)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            if (json.status == 'success') {
+                getTestimonial();
+                castSuccess('Successfully edited your post!');
+                localStorage.setItem('message', testimonialVal);
+            } else {
+                castError('There was an error whilst editing your testimonial!');
+            }
+            submitbtn.textContent = 'SUBMIT';
+            submitbtn.setAttribute('onClick', 'postTestimonial()');
+        })
+}
+function deletePost() {
+    const id = localStorage.getItem('ID');
+    const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ id }) }
+    fetch('/deleteTestimonial', options)
+        .then(res => res.json())
+        .then(json => {
+            if (json.status == 'success') {
+                getTestimonial();
+                $('.editbtn, .deletebtn').remove();
+                castSuccess('Successfully deleted your post!');
+            } else {
+                castError('Error! There was an error trying to delete your post!');
+            }
+        })
 }
 
 function getTestimonial() {
     fetch('/getTestimonial')
-    .then(res => res.json())
-    .then(json => {
-        for(var i = 0; i <= 4; i++ ) {
-            document.getElementById(`namefield${i}`).textContent = json[`${i}`].name;
-            document.getElementById(`countryfield${i}`).textContent = json[`${i}`].country;
-            document.getElementById(`messagefield${i}`).textContent = json[`${i}`].testimonial;
-        }
-    })
+        .then(res => res.json())
+        .then(json => {
+            for (var i = 0; i <= json.length - 1; i++) {
+                if (localStorage.getItem('message') == json[`${i}`].testimonial) {
+                    $(`<button class="editbtn" onclick="checkId('editPost()')">Edit</button> <button class="deletebtn" onclick="checkId('deletePost()')">Delete</button>`).appendTo(`#buttons${i}`);
+                }
+                document.getElementById(`namefield${i}`).textContent = json[`${i}`].name;
+                document.getElementById(`countryfield${i}`).textContent = '(' + json[`${i}`].country + ')';
+                document.getElementById(`messagefield${i}`).textContent = json[`${i}`].testimonial;
+            }
+        })
+        .catch(err => castError('An error occured whilst fetching testimonials'));
 }
 // Scroller //
 new ScrollBooster({
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
 const Datastore = require('nedb');
-const database = new Datastore({ filename: 'testimonials', autoload: true});
+const database = new Datastore({ filename: 'testimonials', autoload: true });
 const rateLimit = require("express-rate-limit");
 require('dotenv').config();
 const port = process.env.PORT;
@@ -17,53 +17,80 @@ const postLimiter = rateLimit({
     windowMs: 60 * 60 * 1000,
     max: 1,
     message:
-      "bruh"
+        "bruh"
 });
 
 app.post('/postTestimonial', (req, res) => {
     const name = req.body.name,
-          country = req.body.country,
-          testimonial = req.body.testimonial;
-    try {
-        var entry = {
-            name: name,
-            country: country,
-            testimonial: testimonial,
-            postedAt: new Date()
-        }
-        database.insert(entry, (err, doc) => {
-            res.json({
-                status: 'success',
-                id: doc._id
-            })
-            if(err) {
-                res.json({ status: 'failed', message: 'An internal error has occured, please contact us ASAP!' })
+        country = req.body.country,
+        testimonial = req.body.testimonial;
+    if (testimonial.length <= 91) {
+        try {
+            var entry = {
+                name: name,
+                country: country,
+                testimonial: testimonial,
+                postedAt: new Date()
             }
-        });
-    } catch {
-        res.json({status: 'failed to post entry to the database!'})
+            database.insert(entry, (err, doc) => {
+                res.json({
+                    status: 'success',
+                    id: doc._id
+                })
+                if (err) {
+                    res.json({ status: 'failed', message: 'An internal error has occured, please contact us ASAP!' })
+                }
+            });
+        } catch {
+            res.json({ status: 'failed', message: 'Failed to post to server.' })
+        }
+    } else {
+        res.json({
+            status: 'failed!',
+            message: 'The length of your testimonial is too long!'
+        })
     }
 })
 
+app.post('/checkID', (req, res) => {
+    const id = req.body.id;
+    database.find({ _id: id }, (err, data) => {
+        if (data) {
+            res.json({ status: 'success', data: data });
+        } else {
+            res.json({ status: 'error', message: 'We couldn\'t find that ID!' })
+        }
+    })
+})
 // Also works
 app.get('/getTestimonial', (req, res) => {
-    database.find({}).sort({postedAt: -1}).limit(5).exec((err, data)=>{
-        for (var i = 0, len = data.length; i < len; i++) 
+    database.find({}).sort({ postedAt: -1 }).limit(5).exec((err, data) => {
+        for (var i = 0, len = data.length; i < len; i++)
             delete data[i]._id;
         res.json(data.slice(Math.max(data.length - 5, 0)))
     })
 })
 
-// Works
 app.post('/changeTestimonial', (req, res) => {
-    const name = req.body.name,
-          country = req.body.country,
-          testimonial = req.body.testimonial,
-          _id = req.body.id;
-    database.find({ _id: _id}, err => {
-        database.update({ _id: _id }, { $set: { 'name': name, 'country': country, 'testimonial': testimonial }}, err => {
-            if(err) console.log(err);
+    const name = req.body.nameVal,
+        country = req.body.countryVal,
+        testimonial = req.body.testimonialVal,
+        _id = req.body.id;
+    database.find({ _id: _id }, err => {
+        database.update({ _id: _id }, { $set: { 'name': name, 'country': country, 'testimonial': testimonial } }, (err, data) => {
+            res.json({ status: 'success' })
+            if (err) console.log(err);
         })
-        if(err) { res.json({ status: 'Incorrect ID! Please set the correct one!' }) }
+        if (err) { res.json({ status: 'Incorrect ID! Please set the correct one!' }) }
+    })
+})
+
+app.post('/deleteTestimonial', (req, res) => {
+    const _id = req.body.id;
+    database.find({ _id: _id }, err => {
+        database.remove({ _id: _id }, err => {
+            res.json({ status: 'success' })
+            if (err) console.log(err);
+        })
     })
 })
